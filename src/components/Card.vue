874ff54d980062e026ebf4869903a48f6fc0f268
Thomas Chapuis <chapuis.t@gmail.com>
2020-10-11 18:33:31 +0200

    Added Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="rounded overflow-hidden shadow mb-4 bg-white dark:bg-gray-700">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">{{ sentence.question }}</div>
+      <p class="text-gray-700 dark:text-gray-200">
+        {{ sentence.answer }}
+      </p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <div class="flex justify-between">
+        <button @click="deleteSentence(sentence.id)" class="text-red-500 focus:outline-none">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+          </svg>
+        </button>
+        <button @click.prevent="editSentence(sentence)" class="text-teal-600 focus:outline-none">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    sentence: Object,
+  },
+  methods: {
+    editSentence(sentence) {
+      const el = this.$parent.$el.querySelector('#question-form');
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth' });
+      }
+
+      this.$parent.sentence.question = sentence.question;
+      this.$parent.sentence.answer = sentence.answer;
+      this.$parent.editId = sentence.id;
+    },
+    deleteSentence(id) {
+      if (window.confirm('Confirmer la suppression')) {
+        this.$store.commit('sentences/delete', id);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -26,38 +26,18 @@
     </form>
     <hr class="mb-5">
     <div>
-      <div class="rounded overflow-hidden shadow mb-4 bg-white dark:bg-gray-700" v-for="sentenceElem in sentences" :key="sentenceElem.id">
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">{{ sentenceElem.question }}</div>
-          <p class="text-gray-700 dark:text-gray-200">
-            {{ sentenceElem.answer }}
-          </p>
-        </div>
-        <div class="px-6 pt-4 pb-2">
-          <div class="flex justify-between">
-            <button @click="deleteSentence(sentenceElem.id)" class="text-red-500 focus:outline-none">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-              </svg>
-            </button>
-            <button @click.prevent="editSentence(sentenceElem)" class="text-teal-600 focus:outline-none">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-
+      <Card v-for="sentenceElem in sentences" :key="sentenceElem.id" :sentence="sentenceElem"></Card>
     </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import Card from '@/components/Card.vue';
 
 export default {
   name: 'Admin',
+  components: { Card },
   data() {
     return {
       editId: null,
@@ -80,33 +60,10 @@ export default {
         }
       }
     },
-    editSentence(sentence) {
-      const el = this.$el.querySelector('#question-form');
-      if (el) {
-        el.scrollIntoView({ behavior: 'smooth' });
-      }
-
-      this.sentence.question = sentence.question;
-      this.sentence.answer = sentence.answer;
-      this.editId = sentence.id;
-    },
-    deleteSentence(id) {
-      if (window.confirm('Confirmer la suppression')) {
-        this.$store.commit('sentences/delete', id);
-      }
-    },
     resetSentence() {
       this.sentence.question = '';
       this.sentence.answer = '';
       this.editId = null;
-    },
-    scrollTop() {
-      this.intervalId = setInterval(() => {
-        if (window.pageYOffset === 0) {
-          clearInterval(this.intervalId);
-        }
-        window.scroll(0, window.pageYOffset - 50);
-      }, 20);
     },
   },
   computed: {
