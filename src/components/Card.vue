032948bfcc59c2dcbd0dbe8f38770d6b99818c88
Thomas Chapuis <chapuis.t@gmail.com>
2020-10-16 16:35:01 +0200

    Added modal component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,7 +6,7 @@
     </div>
     <div class="px-6 pt-4 pb-2">
       <div class="flex justify-between">
-        <button @click="deleteSentence(sentence.id)" class="text-red-500 focus:outline-none">
+        <button @click="$refs.deleteConfirm.openModal()" class="text-red-500 focus:outline-none">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
           </svg>
@@ -18,12 +18,24 @@
         </button>
       </div>
     </div>
+    <modal ref="deleteConfirm">
+      <template v-slot:body>
+        <p class="text-xl text-center">Veuillez confirmer la suppression</p>
+      </template>
+      <template v-slot:footer>
+          <button class="hover:text-teal-600 focus:outline-none" @click="$refs.deleteConfirm.closeModal()">Annuler</button>
+          <button class="btn" @click="deleteSentence(sentence.id)">Supprimer</button>
+      </template>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from '@/components/Modal.vue';
+
 export default {
   name: 'Card',
+  components: { Modal },
   props: {
     sentence: Object,
   },
@@ -38,9 +50,8 @@ export default {
     },
     deleteSentence(id) {
       if (id) {
-        if (window.confirm('Confirmer la suppression')) {
-          this.$store.commit('sentences/delete', id);
-        }
+        this.$refs.deleteConfirm.closeModal();
+        this.$store.commit('sentences/delete', id);
       }
     },
   },
---
+++ src/components/Modal.vue
@@ -0,0 +1,83 @@
+<template>
+  <transition name="modal-fade">
+    <div class="modal" v-if="show">
+      <div class="modal-backdrop" @click="closeModal()"/>
+
+      <div class="modal-dialog">
+        <div class="modal-header">
+          <slot name="header"/>
+        </div>
+
+        <div class="modal-body">
+          <slot name="body"/>
+        </div>
+
+        <div class="modal-footer">
+          <slot name="footer"/>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    closeModal() {
+      this.show = false;
+    },
+    openModal() {
+      this.show = true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal {
+  @apply fixed top-0 right-0 left-0 bottom-0 overflow-x-hidden overflow-y-auto z-10;
+}
+
+.modal-backdrop {
+  @apply fixed top-0 right-0 left-0 bottom-0;
+  background-color: rgba(0, 0, 0, 0.3);
+  z-index: 1;
+}
+
+.modal-dialog {
+  @apply relative bg-white flex flex-col rounded mx-auto my-12 p-4;
+  width: 600px;
+  z-index: 2;
+}
+
+.modal-header {
+  @apply flex items-start justify-between;
+
+}
+
+.modal-body {
+  @apply overflow-auto flex flex-col items-stretch mb-4;
+
+}
+
+.modal-footer {
+  @apply flex justify-between pt-4;
+}
+
+.modal-fade-enter,
+.modal-fade-leave-active {
+  opacity: 0;
+}
+
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity .4s ease
+}
+
+</style>
